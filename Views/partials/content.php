3789751813b70ae5fcc34824b58a166d3b83626f
Cris-NTL <natolicristian92@gmail.com>
2023-05-18 23:04:15 +0200

    Add: more products (in progress)

--- Views/partials/content.php
+++ Views/partials/content.php
@@ -4,12 +4,12 @@
             <?php foreach ($products as $key => $product) { ?>
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
+                        <img src="<?php echo $product->image; ?>" class="card-img-top" alt="...">
+                        <div class="card-body text-center">
                             <h5 class="card-title"><?php echo $product->name; ?></h5>
                             <i class="<?php echo $product->category->icon; ?>"></i>
                             <p>Category: <?php echo $product->category->name ?></p>
-                            <p>€ <?php echo $product->price ?></p>
+                            <p>Price: € <?php echo $product->price ?></p>
                             <p><?php echo $product->getDetails(); ?></p>
                             <?php if (get_class($product) === "Food") { ?>
                                 <p><?php echo $product->getExpiration(); ?></p>
--- db.php
+++ db.php
@@ -10,15 +10,15 @@ $dog_category = new Category("dog", "fa-solid fa-dog");
 $cat_category = new Category("cat", "fa-solid fa-cat");
 // var_dump($cat_category);
 
-$my_cat = new Food("My Cat", 2, $cat_category, "", "2023-08-21");
+$my_cat = new Food("My Cat", 2.50, $cat_category, "https://m.media-amazon.com/images/I/71Tvyq+SmUL.jpg", "2023-08-21");
 // var_dump($my_cat);
-$my_dog = new Food("My Dog", 3, $dog_category, "", "2023-4-14");
+$my_dog = new Food("My Dog", 6.50, $dog_category, "https://happydog_it_sb.cstatic.io/440x440/f/69110/480x480/c90b13149a/hd-vet-new-product-slider-packshots-sensible-11kg-300g.png", "2023-4-14");
 // var_dump($my_dog);
 
-$duck = new Toy("Duck", "2.99", $dog_category, "", "wood", "duck");
+$duck = new Toy("Duck", "2.99", $dog_category, "https://cdn.shopify.com/s/files/1/0940/6942/products/duck.jpg?v=1600329317", "Wood", "Duck-Toy");
 // var_dump($duck);
 
-$mouse = new Toy("Mouse", "1.99", $cat_category, "", "plastic", "mouse");
+$mouse = new Toy("Mouse", "1.99", $cat_category, "https://5.imimg.com/data5/SELLER/Default/2021/2/LD/JE/UR/12304017/sticky-rat-toy-500x500.jpg", "Plastic", "Mouse-Toy");
 // var_dump($mouse);
 
 $products =
